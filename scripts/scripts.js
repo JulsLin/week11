f4e477ad96595193b8590eced90ce626b11adfd4
Juls Lin <julianna.lin4@gmail.com>
2023-03-20 14:22:40 -0700

    define function declaration and expression

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+function greet(username) {
+    console.log(`Hello ${username}`);
+}
+greet('Juls')
+
+let greet = function(name) {
+    console.log(`Hello ${name}`)
+}
+greet("Neda")
+greet("John")
